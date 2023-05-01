40a009a89affcad792edcb10e9abb8017b4152bd
Marinario <123278184+ilbuono-mosharro@users.noreply.github.com>
2023-05-01 23:03:44 +0200

    update arrays folder

--- 6-arrays/examples.js
+++ 6-arrays/examples.js
@@ -12,3 +12,35 @@ console.log(users)
 users.forEach(function(user) {
     console.log(user)
 })
+
+const nums = [1, 2, 3, 4, 5]
+
+// Array filter 
+
+const results = nums.filter(function(num) {
+    return num > 3
+})
+console.log(results)
+
+// Array find
+
+const arrFind = users.find(function(user) {
+    return user === "Tom"
+})
+
+console.log(arrFind)
+
+// Array map
+
+const mapArray = nums.map(function(num) {
+    return num * 2
+})
+console.log(mapArray)
+
+// Array includes
+
+console.log(nums.includes(3))
+
+// Array join
+
+console.log(users.join(","))
